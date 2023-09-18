19d693d69c637c641748ddd58e689496a63532dc
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-09-18 22:07:22 +0300

    task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,6 @@ import throttle from 'lodash.throttle';
 const iframe = document.getElementById('vimeo-player');
 console.log(iframe);
 const player = new Vimeo(iframe);
-
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(data) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const KEY_FEEDBACK = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+let formSubmit = {};
+populateMessageOutput();
+function onFormInput(e) {
+  formSubmit = {
+    email: refs.input.value,
+    message: refs.textarea.value,
+  };
+  localStorage.setItem(KEY_FEEDBACK, JSON.stringify(formSubmit));
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (!refs.input.value || !refs.textarea.value) {
+    alert('Заповніть всі поля!');
+    return;
+  }
+  console.log(formSubmit);
+  localStorage.removeItem(KEY_FEEDBACK);
+  e.currentTarget.reset();
+}
+
+function populateMessageOutput() {
+  const formData = localStorage.getItem(KEY_FEEDBACK);
+  //   console.log(formData);
+  if (formData) {
+    formSubmit = JSON.parse(formData);
+    refs.input.value = formSubmit.email;
+    refs.textarea.value = formSubmit.message;
+  }
+}
